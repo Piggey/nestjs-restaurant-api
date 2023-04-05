d072d9d2d035bf12a6207ed9d769c9c24208e9ad
Piggey <pepcio3320@gmail.com>
2023-04-05 12:54:19 +0200

    use `ValidationPipe` globally

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
@@ -6,6 +6,8 @@ const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(PORT);
   Logger.log(`Started server on http://localhost:${PORT}/`);
 }
