704d8b521b43ee084cbda330524bfe0ffb66ca8d
Piggey <pepcio3320@gmail.com>
2023-04-05 13:55:00 +0200

    fix tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule.register({}), PrismaModule, ConfigModule.forRoot()],
       providers: [AuthService],
     }).compile();
 
