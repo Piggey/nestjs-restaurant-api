89ea778f04b8d8132330650cf08b4fd0df50bb4b
Piggey <pepcio3320@gmail.com>
2023-04-05 13:54:21 +0200

    start implementing `/auth/signin` and `/auth/signup`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserAuthDto } from 'src/user/dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() usr: UserAuthDto) {
+    return this.authService.signUp(usr);
+  }
+
+  @Post('signin')
+  async signIn(@Body() usr: UserAuthDto) {
+    return this.authService.signIn(usr);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserAuthDto } from '../user/dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly db: PrismaService,
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+
+  signIn(usr: UserAuthDto) {
+    throw new Error('Method not implemented.');
+  }
+  signUp(usr: UserAuthDto) {
+    throw new Error('Method not implemented.');
+  }
+}
