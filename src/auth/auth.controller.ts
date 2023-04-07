f04ee48853773bdbb30ce025ce6b5c7edf243c21
Piggey <pepcio3320@gmail.com>
2023-04-07 23:09:08 +0200

    write swagger docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,31 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserAuthDto } from 'src/user/dto';
+import { UserAuthDto } from '../user/dto';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserSignUpResponse } from '../user/response';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiCreatedResponse({
+    description: 'successfully created user and added them to the database.',
+    type: UserSignUpResponse,
+  })
+  @ApiBadRequestResponse({
+    description:
+      "request's body was incorrect. more info in response's `message` attribute",
+  })
+  @ApiConflictResponse({
+    description: 'provided email is not unique',
+  })
   async signUp(@Body() usr: UserAuthDto) {
     return this.authService.signUp(usr);
   }
--- src/user/dto/user-auth.dto.ts
+++ src/user/dto/user-auth.dto.ts
@@ -1,11 +1,17 @@
-import { IsByteLength, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsByteLength, IsEmail, IsString } from 'class-validator';
 
 export class UserAuthDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'f6fc84c9f21c24907d6bee6eec38cabab5fa9a7be8c4a7827fe9e56f245bd2d5',
+    minLength: 64,
+    maxLength: 64,
+  })
   @IsString()
-  @IsNotEmpty()
   @IsByteLength(64, 64)
   passwordHash: string; // SHA-256 encrypted password
 }
