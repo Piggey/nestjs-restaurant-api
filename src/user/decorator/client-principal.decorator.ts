16745485874dbe9e7739f1adeba2b00540375443
Piggey <pepcio3320@gmail.com>
2023-04-13 19:01:24 +0200

    extract decoding `ClientPrincipalDto` from header to separate method

--- src/user/decorator/client-principal.decorator.ts
+++ src/user/decorator/client-principal.decorator.ts
@@ -8,37 +8,43 @@ import { ClientPrincipalDto } from '../dto';
 
 export const ClientPrincipal = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): ClientPrincipalDto => {
-    const req = ctx.switchToHttp().getRequest();
+    const req = ctx.switchToHttp().getRequest<Request>();
 
     if (!req.headers['x-ms-client-principal']) {
       throw new HttpException(
-        'x-ms-client-principal request header not found',
+        'x-ms-client-principal request header not provided',
         HttpStatus.BAD_REQUEST,
       );
     }
 
-    const encoded = Buffer.from(req.headers['x-ms-client-principal'], 'base64');
-    const decoded = encoded.toString();
-
-    const usr: ClientPrincipalDto = JSON.parse(decoded);
-    usr.userRoles = usr.userRoles.filter(
-      (role) => role !== 'anonymous' && role !== 'authenticated',
-    );
-
-    if (usr.userRoles.length === 0) {
+    const client = getClientPrincipalFromHeader(req);
+    if (client.userRoles.length === 0) {
       throw new HttpException(
         'no custom userRole specified',
         HttpStatus.BAD_REQUEST,
       );
     }
 
-    if (usr.userRoles.length > 1) {
+    if (client.userRoles.length > 1) {
       throw new HttpException(
         'too many custom roles specified',
         HttpStatus.BAD_REQUEST,
       );
     }
 
-    return usr;
+    return client;
   },
 );
+
+export const getClientPrincipalFromHeader = (
+  req: Request,
+): ClientPrincipalDto => {
+  const encoded = Buffer.from(req.headers['x-ms-client-principal'], 'base64');
+  const decoded = encoded.toString();
+  const client = JSON.parse(decoded);
+  client.userRoles = client.userRoles.filter(
+    (role) => role !== 'anonymous' && role !== 'authenticated',
+  );
+
+  return client;
+};
