53c1931631f6c62f3a004d4ba434f689542d2783
Piggey <pepcio3320@gmail.com>
2023-04-19 18:57:38 +0200

    remove unnecessary check

--- src/user/decorator/client-principal.decorator.ts
+++ src/user/decorator/client-principal.decorator.ts
@@ -11,15 +11,8 @@ export const CLIENT_PRINCIPAL_HEADER = 'x-ms-client-principal';
 export const ClientPrincipal = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): UserAuthDto => {
     const req = ctx.switchToHttp().getRequest<Request>();
-
-    if (!req.headers[CLIENT_PRINCIPAL_HEADER]) {
-      throw new HttpException(
-        `${CLIENT_PRINCIPAL_HEADER} request header not provided`,
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
     const client = getClientPrincipalFromHeader(req);
+
     if (client.userRoles.length === 0) {
       throw new HttpException(
         'no custom userRole specified',
@@ -42,7 +35,7 @@ export const getClientPrincipalFromHeader = (req: Request): UserAuthDto => {
   if (!req.headers[CLIENT_PRINCIPAL_HEADER])
     throw new HttpException(
       `${CLIENT_PRINCIPAL_HEADER} request header not provided`,
-      HttpStatus.FORBIDDEN,
+      HttpStatus.BAD_REQUEST,
     );
 
   const encoded = Buffer.from(req.headers[CLIENT_PRINCIPAL_HEADER], 'base64');
