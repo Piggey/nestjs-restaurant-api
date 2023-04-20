e963c6b79ceeb75764e0707bec9e2e7de2b23102
Piggey <pepcio3320@gmail.com>
2023-04-20 16:47:20 +0200

    `/auth/signin`: change `200 OK` to `201 CREATED`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,33 +1,27 @@
-import { Controller, Get, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {
   ApiBadRequestResponse,
+  ApiCreatedResponse,
   ApiHeader,
-  ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
-import { UserSignInResponse } from '../user-old/response';
-import {
-  AllowRoles,
-  CLIENT_PRINCIPAL_HEADER,
-  ClientPrincipal,
-} from './decorator';
+import { AllowRoles, ClientPrincipal } from './decorator';
 import { RolesGuard } from './guard';
 import { UserRoles } from './model';
-import { ClientPrincipalDto } from './dto';
+import {
+  ClientPrincipalDto,
+  SWAGGER_CLIENT_PRINCIPAL_HEADER_INFO,
+} from './dto';
+import { UserSignInResponse } from './responses';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiHeader({
-    name: CLIENT_PRINCIPAL_HEADER,
-    description: 'Client principal encoded as base64',
-    example: 'eyJ1c2VySWQiOiAiMjIyMjIyMjIyMiJ9',
-    required: true,
-  })
-  @ApiOkResponse({
+  @ApiHeader(SWAGGER_CLIENT_PRINCIPAL_HEADER_INFO)
+  @ApiCreatedResponse({
     description:
       'successfully signed a user in. returns `true` if new used had to be created in the database, otherwise returns `false`',
     type: UserSignInResponse,
@@ -37,7 +31,6 @@ export class AuthController {
       "request's body was incorrect. more info in response's `message` attribute",
   })
   @Post('signin')
-  @HttpCode(200)
   async signIn(
     @ClientPrincipal() usr: ClientPrincipalDto,
   ): Promise<UserSignInResponse> {
