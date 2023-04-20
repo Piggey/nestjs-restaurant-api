1c6e6da6f969d6566d7898a141bf4ecc1d344416
Piggey <pepcio3320@gmail.com>
2023-04-20 16:52:17 +0200

    add swagger documentation tu GET `/employee`

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,15 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
-import { ApiTags } from '@nestjs/swagger';
-import { ClientPrincipal } from 'src/auth/decorator';
-import { ClientPrincipalDto } from 'src/auth/dto';
+import {
+  ApiBadRequestResponse,
+  ApiForbiddenResponse,
+  ApiHeader,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AllowRoles, ClientPrincipal } from '../auth/decorator';
+import {
+  ClientPrincipalDto,
+  SWAGGER_CLIENT_PRINCIPAL_HEADER_INFO,
+} from '../auth/dto';
 import { FetchEmployeesResponse } from './responses/fetch-employees.response';
+import { RolesGuard } from '../auth/guard';
+import { UserRoles } from '../auth/model';
+import { RequestErrorResponse } from '../app/response';
 
 @ApiTags('employee')
 @Controller('employee')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
+  @ApiHeader(SWAGGER_CLIENT_PRINCIPAL_HEADER_INFO)
+  @ApiOkResponse({
+    description:
+      'returns all hired employees in his restaurant for a `MANAGER` level role, returns all hired employees for `BOSS` level role',
+    type: FetchEmployeesResponse,
+  })
+  @ApiBadRequestResponse({
+    description: 'something went wrong with provided user data',
+    type: RequestErrorResponse,
+  })
+  @ApiForbiddenResponse({
+    description: '`userRole` level is not high enough',
+    type: RequestErrorResponse,
+  })
+  @UseGuards(RolesGuard)
+  @AllowRoles(UserRoles.MANAGER)
   @Get('/')
   async fetchEmployees(
     @ClientPrincipal() user: ClientPrincipalDto,
