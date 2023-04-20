453c442e667cd0cf780859a17b20a41dd36d43cf
Piggey <pepcio3320@gmail.com>
2023-04-20 16:46:04 +0200

    move `UserSignInResponse` to auth folder

--- src/user-old/response/index.ts
+++ src/user-old/response/index.ts
@@ -1,5 +1,4 @@
 export * from './about-user.response';
 export * from './create-employee.response';
 export * from './fetch-employees.response';
-export * from './user-signin.response';
 export * from './fetch-managers.response';
