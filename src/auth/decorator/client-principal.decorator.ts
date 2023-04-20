432f3de618d5415999defeeff0537f03f93fc222
Piggey <pepcio3320@gmail.com>
2023-04-20 16:22:18 +0200

    make sure to also log the errors

--- src/auth/decorator/client-principal.decorator.ts
+++ src/auth/decorator/client-principal.decorator.ts
@@ -2,6 +2,7 @@ import {
   ExecutionContext,
   HttpException,
   HttpStatus,
+  Logger,
   createParamDecorator,
 } from '@nestjs/common';
 import { ClientPrincipalDto } from '../dto/client-principal.dto';
@@ -18,11 +19,14 @@ export const ClientPrincipal = createParamDecorator(
 export const getClientPrincipalFromHeader = (
   req: Request,
 ): ClientPrincipalDto => {
-  if (!req.headers[CLIENT_PRINCIPAL_HEADER])
-    throw new HttpException(
+  if (!req.headers[CLIENT_PRINCIPAL_HEADER]) {
+    const err = new HttpException(
       `${CLIENT_PRINCIPAL_HEADER} request header not provided`,
       HttpStatus.BAD_REQUEST,
     );
+    Logger.error(err);
+    throw err;
+  }
 
   const encoded = Buffer.from(req.headers[CLIENT_PRINCIPAL_HEADER], 'base64');
   const decoded = encoded.toString();
@@ -31,28 +35,37 @@ export const getClientPrincipalFromHeader = (
   try {
     client = JSON.parse(decoded);
   } catch (error) {
-    if (error instanceof SyntaxError)
-      throw new HttpException(
+    if (error instanceof SyntaxError) {
+      const err = new HttpException(
         `could not parse ${CLIENT_PRINCIPAL_HEADER}`,
         HttpStatus.BAD_REQUEST,
       );
+      Logger.error(err);
+      throw err;
+    }
   }
 
   client.userRoles = client.userRoles.filter(
     (role) => role !== 'anonymous' && role !== 'authenticated',
   );
 
-  if (client.userRoles.length === 0)
-    throw new HttpException(
+  if (client.userRoles.length === 0) {
+    const err = new HttpException(
       'no custom userRole specified',
       HttpStatus.BAD_REQUEST,
     );
+    Logger.error(err);
+    throw err;
+  }
 
-  if (client.userRoles.length > 1)
-    throw new HttpException(
+  if (client.userRoles.length > 1) {
+    const err = new HttpException(
       'too many custom roles specified',
       HttpStatus.BAD_REQUEST,
     );
+    Logger.error(err);
+    throw err;
+  }
 
   return client;
 };
