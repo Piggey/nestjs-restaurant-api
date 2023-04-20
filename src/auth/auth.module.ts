c2a86850de70078df7486b415f6f644584a222ab
Piggey <pepcio3320@gmail.com>
2023-04-20 16:44:32 +0200

    get rid of unused dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [JwtModule.register({}), PrismaModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot()],
   controllers: [AuthController],
   providers: [AuthService],
 })
