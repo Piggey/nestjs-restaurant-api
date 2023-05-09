2e22061e15d5fdace2d872de9bf7d30a5fa27e14
Piggey <pepcio3320@gmail.com>
2023-05-09 20:54:34 +0200

    fix tests

--- src/menu/menu.controller.spec.ts
+++ src/menu/menu.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MenuController } from './menu.controller';
 import { MenuService } from './menu.service';
+import { PostgresModule } from '../postgres/postgres.module';
 
 describe('MenuController', () => {
   let controller: MenuController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PostgresModule],
       controllers: [MenuController],
       providers: [MenuService],
     }).compile();
--- src/menu/menu.service.spec.ts
+++ src/menu/menu.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MenuService } from './menu.service';
+import { PostgresModule } from '../postgres/postgres.module';
 
 describe('MenuService', () => {
   let service: MenuService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PostgresModule],
       providers: [MenuService],
     }).compile();
 
