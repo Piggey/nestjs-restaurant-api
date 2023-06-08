72ab3136e1372fa287ee0f72340b500b627991bb
Piggey <pepcio3320@gmail.com>
2023-06-08 18:19:16 +0200

    make employee creatable when creating manager

--- src/manager/dto/create-manager.dto.ts
+++ src/manager/dto/create-manager.dto.ts
@@ -1,19 +1,35 @@
-import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { IsNotEmpty, IsOptional, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
+import { CreateEmployeeDto } from '../../employee/dto/create-employee.dto';
 import { ConnectEmployeeDto } from '../../employee/dto/connect-employee.dto';
 
 export class CreateManagerEmployeeRelationInputDto {
   @ApiProperty({
+    required: false,
+    nullable: true,
+    type: CreateEmployeeDto,
+  })
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateEmployeeDto)
+  create?: CreateEmployeeDto;
+  @ApiProperty({
+    required: false,
+    nullable: true,
     type: ConnectEmployeeDto,
   })
-  @IsNotEmpty()
+  @IsOptional()
   @ValidateNested()
   @Type(() => ConnectEmployeeDto)
-  connect: ConnectEmployeeDto;
+  connect?: ConnectEmployeeDto;
 }
 
-@ApiExtraModels(ConnectEmployeeDto, CreateManagerEmployeeRelationInputDto)
+@ApiExtraModels(
+  CreateEmployeeDto,
+  ConnectEmployeeDto,
+  CreateManagerEmployeeRelationInputDto,
+)
 export class CreateManagerDto {
   @ApiProperty({
     type: CreateManagerEmployeeRelationInputDto,
